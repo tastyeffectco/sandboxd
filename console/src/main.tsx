c5e76232bbe205cb0d0edbdb6903275a7fde7658
Addelouahed EL KASRI <tastyeffectco@gmail.com>
2026-07-10 22:49:00 +0000

    console: static read-only demo mode (VITE_DEMO=1) for sandboxd.io/demo; README links it

--- console/src/main.tsx
+++ console/src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './design.css'
+import { IS_DEMO, installDemo } from './demo'
+import DemoBanner from './DemoBanner'
+
+if (IS_DEMO) installDemo()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    {IS_DEMO && <DemoBanner />}
     <App />
   </React.StrictMode>,
 )
